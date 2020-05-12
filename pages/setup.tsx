67281dfd6ca5feb63105b87677f6e5f36c7e98c4
Michael <mikedev0431@gmail.com>
2020-05-11 22:12:35 -0700

    Added progressbar markup and css styles

--- pages/setup.tsx
+++ pages/setup.tsx
@@ -4,6 +4,7 @@ import { useMutation } from '@apollo/react-hooks';
 import { MUTATION_SETUP } from '../graphql';
 import { SetUpForm } from '../components/Setup';
 import { Button } from '../components/Ui/Button'
+import FadeIn from '../components/Ui/Animation/FadeIn';
 
 const Setup: React.FC = () => {
   const router = useRouter();
@@ -28,13 +29,20 @@ const Setup: React.FC = () => {
       <div className="container">
         <div className="row align-items-center veritcalAlign">
           <div className="col">
-            <h1>Welcome to Orijin.</h1>
-            <SetUpForm />
-            <Button
-              maxWidth={300}
-              mutationLoading={false}
-              float="right"
-              onClick={() => console.log('Next page')}>Next</Button>
+            <FadeIn>
+              <div className="progessbar-container mb-5">
+                <div className="step">1</div>
+                <div className="step">2</div>
+                <div className="step">3</div>
+              </div>
+              <h1>Welcome to Orijin.</h1>
+              <SetUpForm />
+              <Button
+                maxWidth={300}
+                mutationLoading={false}
+                float="right"
+                onClick={() => console.log('Next page')}>Next</Button>
+            </FadeIn>
           </div>
         </div>
       </div>
@@ -49,6 +57,28 @@ const Setup: React.FC = () => {
           h1 {
             font-family: 'Montserrat';
             font-weight: 900;
+          }
+          .progessbar-container {
+            display: flex;
+            justify-content: space-between;
+            padding: 0 3rem;
+          }
+
+          .progessbar-container .step {
+            width: 30px;
+            height: 30px;
+            font-family: 'Montserrat';
+            line-height: 30px;
+            text-align: center;
+            background-color: #eef4fe;
+            border-radius: 22px;
+            webkit-box-shadow: 25px 37px 34px 0px rgba(0,123,255,0.25);
+            -moz-box-shadow: 25px 37px 34px 0px rgba(0,123,255,0.25);
+            box-shadow: 0px 10px 24px 0px rgba(0,123,255,0.30);
+          }
+
+          .progessbar-container .active{
+            border: 1px solid #111b60;
           }
         `}
       </style>
