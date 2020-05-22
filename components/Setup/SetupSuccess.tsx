49d1fa13f2912e27c4429cd44d11cee61bc15f84
Michael <mikedev0431@gmail.com>
2020-05-22 09:30:14 -0700

    Added new object to the steps array. It's the last object for now as it will contain the Success component for the set up process. New html mark up was also added along with style changes to support a dynamic animation type slider form

---
+++ components/Setup/SetupSuccess.tsx
@@ -0,0 +1,13 @@
+export interface SetupSuccessProps {
+
+}
+
+const SetupSuccess: React.SFC<SetupSuccessProps> = () => {
+  return (
+    <div className="form-row mb-4">
+      <p>Awesome Blossom</p>
+    </div>
+  );
+}
+
+export default SetupSuccess;
