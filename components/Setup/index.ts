ba309f257287dc7781f1a44e77d995cea22316d0
Michael <mikedev0431@gmail.com>
2020-05-22 09:32:29 -0700

    Added new export default for SetupSuccess component

--- components/Setup/index.ts
+++ components/Setup/index.ts
@@ -1,3 +1,4 @@
 export { default as SetUpForm } from './SetupForm';
 export { default as SetupStep1 } from './SetupStep1';
 export { default as SetupStep2 } from './SetupStep2';
+export { default as SetupSuccess } from './SetupSuccess';
