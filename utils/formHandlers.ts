6d8e82a9d7f3d94c1f065a83dab7d604ac3d438f
triedbyseven <mikedev0431@gmail.com>
2020-05-30 16:43:42 -0700

    The nextStep handler was extended to update the state errors based on the object schema from yup errors.

--- utils/formHandlers.ts
+++ utils/formHandlers.ts
@@ -1,3 +1,4 @@
+import { setupFormStepOneSchema, setupFormStepTwoSchema } from '..//utils/formValidation';
 import gsap from 'gsap';
 
 const tl = gsap.timeline();
@@ -13,16 +14,62 @@ export const onChangeText = (event, updateValue) => {
 };
 
 
-export const nextStep = (state, steps, updateState) => {
+export const nextStep = async (state, steps, updateState, updateErrors, refs) => {
   if (state.currentStep.index + 1 === state.steps.length) return;
 
-  gsap.to('.active-line', 0.5, { attr: { x2: '100%' }, ease: 'power4' });
-  const newIndex = state.currentStep.index + 1;
-  updateState(prevState => ({ steps: prevState.steps, currentStep: steps[newIndex] }));
+  const values1 = { firstName: refs.firstNameRef.current.value, lastName: refs.lastNameRef.current.value };
+  const values2 = { companyName: refs.companyNameRef.current.value, email: refs.emailRef.current.value, username: refs.usernameRef.current.value, password: refs.passwordRef.current.value };
 
-  tl.to(".step", { y: 50, opacity: 0, ease: 'linear', duration: 0.2 });
-  tl.to(".step", { xPercent: '-=' + 100, ease: 'linear', duration: 0.2 });
-  tl.to(".step", { y: 0, opacity: 1, ease: 'linear', duration: 0.2 });
+  try {
+    const res = state.currentStep.index === 0 ? await setupFormStepOneSchema.validate(values1, { abortEarly: false }) : await setupFormStepTwoSchema.validate(values2, { abortEarly: false })
+
+
+    if (state.currentStep.index === 0) {
+      updateErrors(prevState => ({
+        allErrors: prevState.allErrors,
+        inputErrors: {
+          firstName: false,
+          lastName: false,
+          companyName: null,
+          email: null,
+          username: null,
+          password: null
+        }
+      }));
+    }
+
+    if (state.currentStep.index === 1) {
+      updateErrors(prevState => ({
+        allErrors: prevState.allErrors,
+        inputErrors: {
+          firstName: false,
+          lastName: false,
+          companyName: false,
+          email: false,
+          username: false,
+          password: false
+        }
+      }));
+    }
+
+    gsap.to('.active-line', 0.5, { attr: { x2: '100%' }, ease: 'power4' });
+    const newIndex = state.currentStep.index + 1;
+    updateState(prevState => ({ steps: prevState.steps, currentStep: steps[newIndex] }));
+
+    tl.to(".step", { opacity: 0, ease: 'linear', duration: 0.2 });
+    tl.to(".step", { xPercent: '-=' + 100, ease: 'linear', duration: 0.2 });
+    tl.to(".step", { opacity: 1, ease: 'linear', duration: 0.2 });
+
+    console.log(res);
+  } catch (err) {
+    let inputErrors: any = {};
+
+    for (const key of err.inner) {
+      inputErrors[key.path] = key.path ? true : false;
+    };
+
+    return updateErrors({ allErrors: err.errors, inputErrors });
+  }
 }
 
 export const lastStep = (state, steps, updateState) => {
@@ -35,7 +82,7 @@ export const lastStep = (state, steps, updateState) => {
   const newIndex = state.currentStep.index - 1;
   updateState(prevState => ({ currentStep: steps[newIndex], steps: prevState.steps }));
 
-  tl.to(".step", { y: 50, opacity: 0, ease: 'linear', duration: 0.2 });
+  tl.to(".step", { opacity: 0, ease: 'linear', duration: 0.2 });
   tl.to(".step", { xPercent: '+=' + 100, ease: 'linear', duration: 0.2 });
-  tl.to(".step", { y: 0, opacity: 1, ease: 'linear', duration: 0.2 });
+  tl.to(".step", { opacity: 1, ease: 'linear', duration: 0.2 });
 }
