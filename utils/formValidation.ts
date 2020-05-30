204ef4eaf30ebfd0f4f272fcaecdc2a594cf00e6
triedbyseven <mikedev0431@gmail.com>
2020-05-30 16:41:56 -0700

    We have spit only schema into two yup object schemas. This had to be done so each step can validate it's its grouped input fields. Combining them was causing all the steps to validate unstable.

--- utils/formValidation.ts
+++ utils/formValidation.ts
@@ -1,11 +1,13 @@
 import * as yup from 'yup';
 
-export const setupFormSchema = yup.object().shape({
+export const setupFormStepOneSchema = yup.object().shape({
     firstName: yup.string().required(),
-    lastName: yup.string().required(),
+    lastName: yup.string().required()
+})
+
+export const setupFormStepTwoSchema = yup.object().shape({
     companyName: yup.string().required(),
     email: yup.string().email().required(),
     username: yup.string().required(),
     password: yup.string().required(),
-
 })
