d846fd209091ff7eec061ab960a8f73797eb0b53
triedbyseven <mikedev0431@gmail.com>
2020-05-30 19:26:27 -0700

    Mutation schema updated for our serverside. Four more values were missing, businessName, fullName, username, email and password. Success is the only value returned to keep user data private.

--- graphql/MutationRunSetup.js
+++ graphql/MutationRunSetup.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-    mutation runSetup($success: boolean) {
-        runSetup(success: $success) {
+    mutation runSetup($success: Boolean $businessName: String $fullName: String $username: String $email: String $password: String) {
+        runSetup(success: $success businessName: $businessName fullName: $fullName username: $username email: $email password: $password) {
             success
         }
     }
