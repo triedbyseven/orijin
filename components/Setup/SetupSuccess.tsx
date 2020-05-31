8c97b4a85490f04ce11e980d2e1818f860c80a91
triedbyseven <mikedev0431@gmail.com>
2020-05-30 17:10:51 -0700

    Added new state for our success component. We will conditionally render the SetupSuccess component instead of the form when the we update the state to true.

--- components/Setup/SetupSuccess.tsx
+++ components/Setup/SetupSuccess.tsx
@@ -3,7 +3,7 @@ export interface SetupSuccessProps {
 }
 
 const SetupSuccess: React.SFC<SetupSuccessProps> = () => {
-    return (<h1>Success!</h1>);
+  return (<h1>Success!</h1>);
 }
 
 export default SetupSuccess;
