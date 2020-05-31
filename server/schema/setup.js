1903491a93038c8140b1cb044e58f0cb8eac21cb
triedbyseven <mikedev0431@gmail.com>
2020-05-30 19:35:17 -0700

    Schema type updated to support rest of the data from the client

--- server/schema/setup.js
+++ server/schema/setup.js
@@ -3,6 +3,11 @@ import { gql } from 'apollo-server-micro';
 const setup = gql`
   type Setup {
     success: Boolean
+    businessName: String
+    fullName: String
+    username: String
+    email: String
+    password: String
   }
 `;
 
