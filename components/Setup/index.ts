aa8ca5e8b8cf0c1af2fe0ddee62aec60853e5ff9
triedbyseven <mikedev0431@gmail.com>
2020-05-30 17:07:38 -0700

    Added default export  for SetupSucess and renamed SetUpForm to SetupForm.

--- components/Setup/index.ts
+++ components/Setup/index.ts
@@ -1,4 +1,5 @@
-export { default as SetUpForm } from './SetupForm';
+export { default as SetupForm } from './SetupForm';
+export { default as SetupSuccess } from './SetupSuccess';
 export { default as SetupStep1 } from './SetupStep1';
 export { default as SetupStep2 } from './SetupStep2';
 export { default as SetupStep3 } from './SetupStep3';
