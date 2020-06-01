de48bab723e7c885a29a6001585849746229206a
triedbyseven <mikedev0431@gmail.com>
2020-06-01 15:59:06 -0700

    Added a margin of -1px to make up for the offset on the border within the input elemement

--- components/Ui/Form/InputText.tsx
+++ components/Ui/Form/InputText.tsx
@@ -83,6 +83,7 @@ const InputPrice: React.SFC<InputPriceProps> = forwardRef(
             background-color: #eef4fe;
             border-top-right-radius: 16px;
             border-bottom-right-radius: 16px;
+            margin-left: -1px;
           }
           .form-control:disabled,
           .form-control[readonly] {
