8fae3cbb3fc245492214fddfac2022fc00cc2cc4
triedbyseven <mikedev0431@gmail.com>
2020-06-01 14:29:39 -0700

    Configured the hashPassword method to take in the password as an argument and to generalt a 10 digit salt right before finally hasing the password with bcrypt. We than return the salt.

--- utils/hashPassword.ts
+++ utils/hashPassword.ts
@@ -1,5 +1,10 @@
 import bcrypt from 'bcrypt';
 
-export const hashPassword = () => {
+export const hashPassword = async (password: string) => {
+    const saltRounds = 10;
 
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
 };
