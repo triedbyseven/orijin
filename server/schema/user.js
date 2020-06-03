479347189cd2e98036d38597625b1b64f277bc8a
triedbyseven <mikedev0431@gmail.com>
2020-06-03 10:15:53 -0700

    Added user module for our User type and the AuthPayLoad type. These will be used to help the resolvers process the correct data for authentication.

---
+++ server/schema/user.js
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+
+export const user = gql`
+    type User {
+        id: ID
+        username: String;
+        email: String;
+        password: String;
+    }
+
+    type AuthPayLoad {
+        token: String
+        user: User
+    }
+`;
