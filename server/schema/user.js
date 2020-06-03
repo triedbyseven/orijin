9d09eaeed73413d665e9391e8568fa7204fa811e
triedbyseven <mikedev0431@gmail.com>
2020-06-03 10:50:27 -0700

    Removed semicolumns from the schema type. This was causing it to breakl

--- server/schema/user.js
+++ server/schema/user.js
@@ -3,9 +3,9 @@ import gql from 'graphql-tag';
 export const user = gql`
     type User {
         id: ID
-        username: String;
-        email: String;
-        password: String;
+        username: String
+        email: String
+        password: String
     }
 
     type AuthPayLoad {
