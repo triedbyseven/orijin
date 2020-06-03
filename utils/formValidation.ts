cf1d18de2168802fd2b67f1874b649946f3981aa
triedbyseven <mikedev0431@gmail.com>
2020-06-02 22:27:39 -0700

    Added fullFormSchema object schema to be used for our server side validation mutation resolver.

--- utils/formValidation.ts
+++ utils/formValidation.ts
@@ -3,11 +3,20 @@ import * as yup from 'yup';
 export const setupFormStepOneSchema = yup.object().shape({
     firstName: yup.string().matches(/^[A-Za-z]+$/, { excludeEmptyString: true }).required(),
     lastName: yup.string().matches(/^[A-Za-z]+$/, { excludeEmptyString: true }).required()
-})
+});
 
 export const setupFormStepTwoSchema = yup.object().shape({
     companyName: yup.string().required(),
     email: yup.string().email().required(),
     username: yup.string().matches(/^[A-Za-z0-9]+$/, { excludeEmptyString: true }).min(4).max(25).required(),
     password: yup.string().matches(/^(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[0-9])/).min(5).required(),
-})
+});
+
+export const fullFormSchema = yup.object().shape({
+    firstName: yup.string().matches(/^[A-Za-z]+$/, { excludeEmptyString: true }).required(),
+    lastName: yup.string().matches(/^[A-Za-z]+$/, { excludeEmptyString: true }).required(),
+    companyName: yup.string().required(),
+    email: yup.string().email().required(),
+    username: yup.string().matches(/^[A-Za-z0-9]+$/, { excludeEmptyString: true }).min(4).max(25).required(),
+    password: yup.string().matches(/^(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[0-9])/).min(5).required(),
+});
