89496f7da66960db4e162196fd71727dee983008
triedbyseven <mikedev0431@gmail.com>
2020-06-03 10:57:16 -0700

    Changed response to reflect the AuthPayLoad return type from our server typeDefs

--- graphql/MutationRunSetup.js
+++ graphql/MutationRunSetup.js
@@ -3,7 +3,10 @@ import gql from 'graphql-tag';
 export default gql`
     mutation runSetup($success: Boolean $businessName: String $fullName: String $username: String $email: String $password: String) {
         runSetup(success: $success businessName: $businessName fullName: $fullName username: $username email: $email password: $password) {
-            success
+            token
+            user {
+                username
+            }
         }
     }
 `;
