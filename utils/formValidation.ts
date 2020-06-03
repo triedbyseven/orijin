f5324f65fbf18ab7ddc38ccf0c07a83e68e85d3d
triedbyseven <mikedev0431@gmail.com>
2020-06-02 22:08:34 -0700

    Configured both schemas to better match regular expressions with yups matches method

--- utils/formValidation.ts
+++ utils/formValidation.ts
@@ -1,13 +1,13 @@
 import * as yup from 'yup';
 
 export const setupFormStepOneSchema = yup.object().shape({
-    firstName: yup.string().required(),
-    lastName: yup.string().required()
+    firstName: yup.string().matches(/^[A-Za-z]+$/, { excludeEmptyString: true }).required(),
+    lastName: yup.string().matches(/^[A-Za-z]+$/, { excludeEmptyString: true }).required()
 })
 
 export const setupFormStepTwoSchema = yup.object().shape({
     companyName: yup.string().required(),
     email: yup.string().email().required(),
-    username: yup.string().required(),
-    password: yup.string().required(),
+    username: yup.string().matches(/^[A-Za-z0-9]+$/, { excludeEmptyString: true }).min(4).max(25).required(),
+    password: yup.string().matches(/^(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[0-9])/).min(5).required(),
 })
